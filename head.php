8cf462d050c9ea28bada49f8ff20f5b773c3dfcb
uniyalvikas <uniyalvikas@hotmail.com>
2014-06-12 23:24:21 +0530

    first commit

---
+++ head.php
@@ -0,0 +1,33 @@
+<!-- Begin Header -->
+
+
+<div class="header-wrapper opacity">
+	<div class="header">
+		<!-- Begin Logo -->
+		<div class="logo">
+		    <a href="index.php">
+               	<h2 style="font-family: 'Audiowide', serif; font-size: 30px;">The Vox PoP</h2>
+			</a>
+	    </div>
+		<!-- End Logo -->
+		<!-- Begin Menu -->
+		<div id="menu-wrapper">
+			<div id="menu" class="menu">
+				<ul id="tiny">
+					<li class="active">
+                        <a href="index.php">VOX News</a>
+				    </li>
+					
+					<li>
+                        <a href="columns.php">Who we are?</a></li>
+					
+					<li><a href="contact.php">Contact Us</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="clear"></div>
+		<!-- End Menu -->
+	</div>
+</div>
+
+<!-- End Header -->
